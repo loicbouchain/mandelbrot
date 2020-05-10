98e2b86fe3a0a839b8fd29157eebed55170fd06f
loicbouchain <loic.bouchain@orange.fr>
2020-05-10 09:33:32 +0200

    add partie 1

--- main.cpp
+++ main.cpp
@@ -4,18 +4,35 @@
 #include "./SDLProgram.hpp"
 #include "./Mandelbrot.hpp"
 #include "./SDLException.hpp"
+#include <thread>
+
+void compute(SDLProgram &program, int height, Mandelbrot &mandelbrot)
+{
+    std::chrono::steady_clock::time_point t_begin = std::chrono::steady_clock::now();
+    for (int y = 0; y < height; y++)
+    {
+        program.updateLine(y, mandelbrot.drawLine(y));
+    }
+    std::chrono::steady_clock::time_point t_end = std::chrono::steady_clock::now();
+
+    // Display the total computation time
+    std::cout
+        << "It took "
+        << std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_begin).count()
+        << " milliseconds to compute the fractal." << std::endl;
+}
 
 int main(){
 
     // Initialize a mandelbrot object of given width and height
     int width = 1000;
     int height = 1000;
-    Mandelbrot mandelbrot(width,height,-2.1,0.6,-1.2,1.2,500);
+    Mandelbrot mandelbrot(width,height,-2.1,0.6,-1.2,1.2,1000);
     try{
         
         // Initialize a SDL Program
         SDLProgram program(width,height);
-
+        /*
         // Get a time indicator at the start of the computation
         std::chrono::steady_clock::time_point t_begin = std::chrono::steady_clock::now();
 
@@ -32,9 +49,12 @@ int main(){
         << "It took "
         << std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_begin).count()
         << " milliseconds to compute the fractal." << std::endl;
-
-        // Launch the render loop
+        
+        // Launch the render loop*/
+        std::thread computeThread(compute, std::ref(program), height, std::ref(mandelbrot));
+        
         program.loop();
+        computeThread.join();
     }
     catch(SDLException& e){
         std::cout << e.what() << std::endl;
