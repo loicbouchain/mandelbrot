6639d7b2b415b92edc5ff61ace5498abb7a74154
loicbouchain <loic.bouchain@orange.fr>
2020-05-10 11:52:57 +0200

    add partie 2

--- main.cpp
+++ main.cpp
@@ -6,21 +6,55 @@
 #include "./SDLException.hpp"
 #include <thread>
 
-void compute(SDLProgram &program, int height, Mandelbrot &mandelbrot)
+void worker(SDLProgram &program,int lignedebut, int lignefin, Mandelbrot &mandelbrot,int num_worker)
 {
     std::chrono::steady_clock::time_point t_begin = std::chrono::steady_clock::now();
-    for (int y = 0; y < height; y++)
+    for (int y = lignedebut; y < lignefin; y++)
     {
         program.updateLine(y, mandelbrot.drawLine(y));
+        
     }
     std::chrono::steady_clock::time_point t_end = std::chrono::steady_clock::now();
 
     // Display the total computation time
     std::cout
-        << "It took "
+        << "Le worker a mit  "
+        << std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_begin).count()
+        << " milliseconds to compute the fractal." << std::endl;
+
+
+}
+
+
+void compute(SDLProgram &program, int height, Mandelbrot &mandelbrot, int nb_workers)
+{
+    std::chrono::steady_clock::time_point t_begin = std::chrono::steady_clock::now();
+    std::thread arrayWorkers[nb_workers];
+    int tranche = height/nb_workers;
+    for (int y = 0; y < nb_workers; y++)
+    {
+        int lignedebut = (tranche *y)+1;
+        int lignefin = tranche*(y+1);
+        arrayWorkers[y]=std::thread(worker, std::ref(program), lignedebut,lignefin, std::ref(mandelbrot),y);
+        
+        //arrayWorkers[y]=workerThread;
+    }
+    for (int y = 0; y < nb_workers; y++)
+    {
+        arrayWorkers[y].join();
+    }
+    
+
+    
+    std::chrono::steady_clock::time_point t_end = std::chrono::steady_clock::now();
+
+    // Display the total computation time
+    std::cout
+        << "le tout a fait "
         << std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_begin).count()
         << " milliseconds to compute the fractal." << std::endl;
 }
+
 
 int main(){
 
@@ -51,10 +85,12 @@ int main(){
         << " milliseconds to compute the fractal." << std::endl;
         
         // Launch the render loop*/
-        std::thread computeThread(compute, std::ref(program), height, std::ref(mandelbrot));
-        
-        program.loop();
+        std::thread computeThread(compute, std::ref(program), height, std::ref(mandelbrot),4);
+
+
         computeThread.join();
+        program.loop();
+
     }
     catch(SDLException& e){
         std::cout << e.what() << std::endl;
